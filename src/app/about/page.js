aba9aa228122b9af848576f435c2cbf89e66fafc
Samyak Shreyash <sam.shreyash@gmail.com>
2024-04-10 12:10:43 +0530

    Added About

---
+++ src/app/about/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import AboutCoverSection from '../../components/About/AboutCoverSection';
+import Link from 'next/link';
+import Skills from '../../components/About/Skills';
+
+const Page = () => {
+  return (
+    <>
+    <AboutCoverSection />
+    <Skills />
+    <h2 className='mt-8 font-semibold text-2xl self-start mx-20 text-dark'>
+    Have a project in mind? Reach out to me 📞 from <Link href="/contact" className='!underline'>here</Link> and let's make it happen. 
+    </h2>
+    </>
+  )
+}
+
+export default Page;
